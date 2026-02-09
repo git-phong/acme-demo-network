d4507ca6bd0a39fb8fe2d26459b799866f44562c
Phong Nguyen <phongnguyen@unknowndedd52aedcef.attlocal.net>
2026-02-09 04:16:31 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a template repository used in the [Get Started with HCP Terraform tutorial collection](https://developer.hashicorp.com/terraform/tutorials/cloud-get-started)
+
+This repository creates the AWS network infrastructure for the fictional ACME Ccorp
+
+- `terraform.tf`: Configures Terraform and the AWS provider.
+- `main.tf`: Defines the AWS VPC.
---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "acme-demo-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+}
